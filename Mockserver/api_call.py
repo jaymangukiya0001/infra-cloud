c431f236b6b8c76fb4ccbc44b18a5c70dd061d6c
Jay Mangukiya <jaymangukiya0001@gmail.com>
2022-06-18 12:42:53 +0530

    added text file support

--- Mockserver/api_call.py
+++ Mockserver/api_call.py
@@ -6,6 +6,13 @@ import json
 app = Flask(__name__)
 auth_token = "0ad359081ec6cced83ddfad9626d7ffd35c827ba"
 
+# to open the url_storage.json if it blank than we assign it as empty dict.
+with open("url_storage.json", "r") as f:
+    try:
+        url_storage = json.load(f)
+    except:
+        url_storage = {}
+
 
 @app.route("/api_call/<path:long_url>", methods=["GET"])
 def api_call(long_url):
@@ -14,10 +21,18 @@ def api_call(long_url):
         long_url = long_url that to be shorten
     RETURN:
         shorten_url = shorter version of long url"""
-
-    connection = bitly_api.Connection(access_token=auth_token)
-    shorten_url = connection.shorten(long_url)
-    return {"shorten_url": shorten_url["url"]}
+    if long_url not in url_storage:
+        try:
+            connection = bitly_api.Connection(access_token=auth_token)
+            shorten_url = connection.shorten(long_url)
+            url_storage[long_url] = shorten_url["url"]
+            with open("url_storage.json", "w+") as f:
+                f.write(json.dumps(url_storage))
+            return {"shorten_url": shorten_url["url"]}
+        except Exception as e:
+            return {"error":e}
+    else:
+        return {"shorten_url": url_storage[long_url]}
 
 
 if __name__ == "__main__":
--- mock_server_call.py
+++ mock_server_call.py
@@ -6,7 +6,10 @@ def main():
     input_url = input("enter the url:")
     response = requests.get(f"http://localhost:5000/api_call/{input_url}")
     response=response.json()
-    print(response["shorten_url"])
+    if "error" in response:
+        print(response["error"])
+    else:
+        print(response["shorten_url"])
 
 
 if __name__ == "__main__":
